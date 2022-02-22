2f4bdf57d2c4952f4d0b32ecf2b82cf0d4685396
Kristen <kriscadacio@gmail.com>
2022-02-22 13:17:58 -0500

    commit tree bec7e8b89b4d6365f5b168347dd6a518bed260ff

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = "Kristen";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!"; 
